76c6625200637214b77a92d1ce260c02e8944352
Andreas Sander <andreas.sander@hotmail.de>
2019-10-20 17:36:32 +0200

    Add toggle button

--- src/components/pages/Crops.js
+++ src/components/pages/Crops.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, Fragment } from "react";
 import { withRouter } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import styled from 'styled-components';
 
 import Wrapper from "../Layout/Wrapper";
 import TopBar from "../Layout/TopBar.js";
@@ -11,6 +12,13 @@ import { cropJson } from "../../helper/api";
 import { saveCrops } from "../../helper/localStorage";
 import { setSelected, hasSelected } from "../../helper/array";
 import routes from "../../routes";
+
+const ButtonGroup = styled.div`
+  display: flex;
+  max-width: 20%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
 const CropSelectionPage = ({ history }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -43,6 +51,34 @@ const CropSelectionPage = ({ history }) => {
         <Spinner />
       ) : (
         <Fragment>
+          <ButtonGroup>
+            <Button
+              style={{ width: "100px" }}
+              variant="outlined"
+              color="secondary"
+              onClick={() => {
+                setCrops(
+                  crops.map(crop =>
+                    Object.assign({}, crop, {
+                      selected: "selected" in crop ? !crop.selected : true
+                    })
+                  )
+                );
+              }}
+            >
+              Toggle
+            </Button>
+            <Button
+              color="primary"
+              disabled={!hasSelected(crops)}
+              onClick={() => {
+                history.push(routes.plan);
+              }}
+            >
+              Weiter
+            </Button>
+          </ButtonGroup>
+
           <List
             withCheckbox={true}
             items={crops.map(type =>
@@ -54,15 +90,6 @@ const CropSelectionPage = ({ history }) => {
             )}
             onSelect={onListSelect}
           />
-          <Button
-            color="primary"
-            disabled={!hasSelected(crops)}
-            onClick={() => {
-              history.push(routes.plan);
-            }}
-          >
-            Weiter
-          </Button>
         </Fragment>
       )}
     </Wrapper>

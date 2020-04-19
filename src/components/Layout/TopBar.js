fd046ff0295ce0e967f9a8853244b197de57d625
Andreas Sander <andreas.sander@hotmail.de>
2020-04-19 17:33:56 +0200

    chore(cleanup)

--- src/components/Layout/TopBar.js
+++ src/components/Layout/TopBar.js
@@ -3,8 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(theme => ({
@@ -23,14 +21,6 @@ const TopBar = ({ title }) => {
   return (
     <AppBar position="static">
       <Toolbar>
-        {/* <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton> */}
         <Typography variant="h6" className={classes.title}>
           {title}
         </Typography>
